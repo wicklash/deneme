f5b3a707b71440de88c7b7377752981495b1429d
Yusuf Eren Hoşgör <wicklash77@gmail.com>
2023-10-12 18:42:38 +0300

    toplama işlemi yapıldı

---
+++ OOP_PRE_LAB_1.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <fstream>
+using namespace std;
+int main() {
+    ifstream inputFile("input.txt");
+
+    if (!inputFile.is_open()) {
+        cerr << "Failed to open input.txt" << endl;
+        return 1;
+    }
+
+    int count, number, sum = 0;
+
+    inputFile >> count; 
+
+    for (int i = 0; i < count; i++) {
+        inputFile >> number; 
+        sum += number;
+    }
+
+    inputFile.close();
+
+    cout << "Sum of " << count << " integers: " << sum << endl;
+
+    return 0;
+}
